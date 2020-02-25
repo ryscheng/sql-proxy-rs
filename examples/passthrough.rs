4d892c98b74f6961e5b3dd9baa774b863d8bde5f
Raymond Cheng <me@raymondcheng.net>
2020-02-25 08:20:47 -0800

    change default addr

--- examples/passthrough.rs
+++ examples/passthrough.rs
@@ -34,7 +34,7 @@ async fn main() {
   // determine address for the proxy to bind to
   let bind_addr = env::args().nth(1).unwrap_or("0.0.0.0:3306".to_string());
   // determine address of the MariaDB instance we are proxying for
-  let db_addr = env::args().nth(2).unwrap_or("127.0.0.1:3306".to_string());
+  let db_addr = env::args().nth(2).unwrap_or("db:3306".to_string());
 
   let mut server = mariadb_proxy::server::Server::new(bind_addr.clone(), db_addr.clone()).await;
   info!("Proxy listening on: {}", bind_addr);
