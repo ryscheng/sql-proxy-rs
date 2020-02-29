7850d36c63ec42b812334901a2fead7e3ef566d7
Raymond Cheng <me@raymondcheng.net>
2020-02-29 13:24:51 -0800

    nit

--- scripts/docker-sqlclient.sh
+++ scripts/docker-sqlclient.sh
@@ -6,5 +6,5 @@ docker run -it \
   --rm \
   --network devnet \
   mariadb \
-  mysql --host=rust-dev --user=root --password=devpassword testdb
+  mysql --host=rust-dev --user=root --password=devpassword 
   #bash
